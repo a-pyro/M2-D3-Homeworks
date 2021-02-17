a79fc2a445f4b10a57f3661a62c9ad09a69c4730
ardigermenji <ardi.germenji@gmail.com>
2021-02-17 19:18:34 +0100

    adds onscrollto the page  with AOS library

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 console.log('Hi there! 🔥');
+AOS.init();
 const shit = '💩',
   fire = '🔥',
   rocket = '🚀',
   poudzo = '👍🏻';
 
 const nav = document.querySelector('nav');
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', showNav);
+
+function showNav() {
+  console.log(pageYOffset);
   if (pageYOffset > 250) {
-    if (nav.classList.contains('backdrop-blur')) {
-      return;
-    } else {
-      nav.style.transform = 'translateY(-200px)';
-    }
+    if (nav.classList.contains('backdrop-blur')) return;
+    nav.style.transform = 'translateY(-200px)';
     setTimeout(() => {
-      nav.style.transform = 'translateY(0)';
       nav.classList.remove('bg-transparent');
       nav.classList.add('backdrop-blur');
+      nav.style.transform = 'translateY(0)';
     }, 100);
   }
+
   if (pageYOffset === 0) {
     nav.style.transform = 'translateY(-200px)';
     setTimeout(() => {
@@ -26,4 +28,4 @@ window.addEventListener('scroll', () => {
       nav.classList.remove('backdrop-blur');
     }, 100);
   }
-});
+}
