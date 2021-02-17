34ecb7eb39579bfe7ea619fbec50d2933b7ba319
ardigermenji <ardi.germenji@gmail.com>
2021-02-17 18:12:51 +0100

    adds onscroll to nav

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ const shit = '💩',
   fire = '🔥',
   rocket = '🚀',
   poudzo = '👍🏻';
+
+const nav = document.querySelector('nav');
+window.addEventListener('scroll', () => {
+  if (pageYOffset > 250) {
+    if (nav.classList.contains('backdrop-blur')) {
+      return;
+    } else {
+      nav.style.transform = 'translateY(-200px)';
+    }
+    setTimeout(() => {
+      nav.style.transform = 'translateY(0)';
+      nav.classList.remove('bg-transparent');
+      nav.classList.add('backdrop-blur');
+    }, 100);
+  }
+  if (pageYOffset === 0) {
+    nav.style.transform = 'translateY(-200px)';
+    setTimeout(() => {
+      nav.style.transform = 'translateY(0)';
+      nav.classList.add('bg-transparent');
+      nav.classList.remove('backdrop-blur');
+    }, 100);
+  }
+});
